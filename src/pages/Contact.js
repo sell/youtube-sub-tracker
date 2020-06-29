6d842ffa295c46016807d8574bcddf2268dc7443
Jay <34258275+sell@users.noreply.github.com>
2020-06-29 07:28:30 -0400

    adding form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,14 +3,17 @@ import React from 'react';
 function Contact() {
     return (
         <div className="form">
-            <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+            <form name="contact" method="post">
+                <input type="hidden" name="form-name" value="contact" />
                 <p>
-                    <label>Your Name: <input type="text" name="name" /></label>   
+                    <label>Your Name: <input type="text" name="name"/></label>
                 </p>
                 <p>
-                    <label>Your Email: <input type="email" name="email" /></label>
+                    <label>Your Email: <input type="email" name="email"/></label>
                 </p>
-                
+                <p>
+                    <label>Message: <textarea name="message"></textarea></label>
+                </p>
                 <p>
                     <button type="submit">Send</button>
                 </p>
