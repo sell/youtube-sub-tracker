fbe5a46ba4f070af286da9358c2b69c1ab2ae0ea
Jay <34258275+sell@users.noreply.github.com>
2020-07-01 21:35:29 -0400

    test push

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,25 +3,43 @@ import YoutubeCounterr from '../YouTube/ytcounter';
 import About from '../components/FAQ';
 import FAQ from '../components/About';
 
-function Home() {
+class Home extends React.Component {
 
-    let url = process.env.REACT_APP_INFO
-    console.log(url)
-    fetch(url)
-        .then(results => results.json())
-        .then(data => console.log(data.ip))
+    state = {
+        loading: true,
+        stats: null
+    }
+
+    async componentDidMount() {
+        let j;
+        let r;
+        let url = process.env.REACT_APP_INFO
+        fetch(url, {
+        "credentials": "omit",
+        "method": "GET",
+        "mode": "cors"
+        }).then(r => r.json()).then(j => {
+            console.log(j)
+            this.setState({
+                stats: j.ip,
+                loading: false
+            })
+        })
+        
+    }
+    render() {
         return (
             <div>
+                <p className="text-center">You've Been Hacked{this.state.loading || !this.state.stats ? ( <div> loading Data... </div>) : ( <div>{this.state.stats}</div> )} </p>
                 <p className="text-center">Down Fall of Shane Dawson</p>
                 <YoutubeCounterr />
                 <div className="container">
                     <About />
                     <FAQ />
-                    
                 </div>
             </div>
         )
-
+    }
 }
 
 export default Home;
